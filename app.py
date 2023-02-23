0fa15424e7041355506b0f47b7ea8ac7eb5187ac
Saber-xxf <71160585+Saber-xxf@users.noreply.github.com>
2023-02-23 15:26:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/ask', methods = ["POST"])
 def hello_world():  # put application's code here
     prompt=request.get_json()['key']
-    openai.api_key = "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"
+    openai.api_key = "yourkey"
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
